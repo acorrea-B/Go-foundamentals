807a937b01119aa9e89a8f4be6db6d57f4b0059b
Janier Alejandro Banguera Correa <aljyque95@gmail.com>
2025-06-24 12:22:11 -0500

    Refactor main function to improve structure and readability of user interactions

--- bank/main.go
+++ bank/main.go
@@ -4,48 +4,107 @@ import "fmt"
 
 func main() {
 	var accountBalance = 10000.0
+	// condicionalBankOptions(accountBalance)
+	for {
 
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Desposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Desposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println("Your account balance is: ", accountBalance)
-	}
+		switch choice {
+		case 1:
+			fmt.Println("Your account balance is: ", accountBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Print("Your deposit: ")
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+		case 3:
+			var withdrawAmount float64
+			fmt.Print("Withdwa amount: ")
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
 
-	if choice == 2 {
-		var depositAmount float64
-		fmt.Print("Your deposit: ")
-		fmt.Scan(&depositAmount)
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				continue
+			}
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+		default:
+			fmt.Println("Good bye!")
+			fmt.Println("Thanks for visit your bank.")
 			return
 		}
-		accountBalance += depositAmount
-		fmt.Println("Balance updated! New amount: ", accountBalance)
-	} else if choice == 3 {
-		var withdrawAmount float64
-		fmt.Print("Withdwa amount: ")
-		fmt.Scan(&withdrawAmount)
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
-		}
 
-		if withdrawAmount > accountBalance {
-			fmt.Println("Invalid amount. You can't withdraw more than you have.")
-			return
-		}
-		accountBalance -= withdrawAmount
-		fmt.Println("Balance updated! New amount: ", accountBalance)
-	} else {
-		fmt.Println("Good bye!")
 	}
 }
+
+/*
+func condicionalBankOptions(accountBalance float64) {
+	for {
+
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Desposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		if choice == 1 {
+			fmt.Println("Your account balance is: ", accountBalance)
+			continue
+		}
+
+		if choice == 2 {
+			var depositAmount float64
+			fmt.Print("Your deposit: ")
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+		} else if choice == 3 {
+			var withdrawAmount float64
+			fmt.Print("Withdwa amount: ")
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				continue
+			}
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+		} else {
+			fmt.Println("Good bye!")
+			break
+		}
+	}
+	fmt.Println("Thanks for visit your bank.")
+}
+*/
