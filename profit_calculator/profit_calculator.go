d534aa95a9cbaba3ce3828282aa1ad987f7a35d8
Janier Alejandro Banguera Correa <aljyque95@gmail.com>
2025-06-24 08:33:14 -0500

    Refactor main function to streamline user input handling and improve readability

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -3,25 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := getUserInput("Input Revenue: ")
+	expenses := getUserInput("Input expenses: ")
+	taxRate := getUserInput("Input Tax Rate: ")
 
-	fmt.Print("Input Revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Input expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Input Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Println("Ebt result: ", ebt)
 	fmt.Println("Profit Result", profit)
-	fmt.Printf("Ratio Result: %v", ratio)
+	fmt.Printf("Ratio Result: %0.2f", ratio)
 
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) float64 {
+	var inputUser float64
+	fmt.Print(infoText)
+	fmt.Scan(&inputUser)
+	return inputUser
 }
