88dc5e98ce886d209fa51fcbeb84d68fbd6e482f
Janier Alejandro Banguera Correa <aljyque95@gmail.com>
2025-06-30 19:35:05 -0500

    Refactor user management by moving User struct and methods to a new user package

--- structs/structs.go
+++ structs/structs.go
@@ -2,39 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
 
-type User struct {
-	firstName string
-	lastName  string
-	birthdate string
-	createdAt time.Time
-}
-
-func (u *User) String() string {
-	return fmt.Sprintf("Name: %s %s, Birthdate: %s, Created At: %s", u.firstName, u.lastName, u.birthdate, u.createdAt.Format(time.RFC3339))
-}
-
-func (u *User) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) *User {
-	return &User{
-		firstName: firstName,
-		lastName:  lastName,
-		birthdate: birthdate,
-		createdAt: time.Now(),
-	}
-}
-
 func main() {
-	var appUser *User
-	appUser = newUser("John", "Doe", "1990-01-01")
+	var appUser *user.User
+	appUser, err := user.NewUser("John", "Doe", "1990-01-01")
 
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+		return
+	}
 	fmt.Println(appUser.String())
-	appUser.clearUserName()
+	appUser.ClearUserName()
 	fmt.Println(appUser.String())
 }
---
+++ structs/user/user.go
@@ -0,0 +1,37 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+// Method for User struct, prints the user details
+func (u *User) String() string {
+	return fmt.Sprintf("Name: %s %s, Birthdate: %s, Created At: %s", u.firstName, u.lastName, u.birthdate, u.createdAt.Format(time.RFC3339))
+}
+
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+// NewUser is a constructor function that initializes a User instance.
+func NewUser(firstName, lastName, birthdate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("all fields must be provided")
+	}
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
